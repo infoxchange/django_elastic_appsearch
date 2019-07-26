029bcd27f6718240ae77662262bc25d884caf77c
Rasika Amaratissa <ramaratissa@infoxchange.org>
2019-07-26 15:55:31 +1000

    Clean the setup.py file

--- setup.py
+++ setup.py
@@ -62,15 +62,12 @@ setup(
     keywords='django_elastic_appsearch',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
