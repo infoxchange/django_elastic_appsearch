daa3539f490806305d17eb2ffd25ca8968b25d9c
Rasika Amaratissa <ramaratissa@infoxchange.org>
2019-07-26 16:05:54 +1000

    Hard code version on setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django_elastic_appsearch',
-    version=version,
+    version='0.2.0',
     description="""Integrate your Django Project with Elastic App Search with ease.""",
     long_description=readme + '\n\n' + history,
     author='Rasika Amaratissa',
