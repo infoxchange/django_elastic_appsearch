d6079b783440f881a8cc47cc1aee6ef15c5ca5a8
Rasika Amaratissa <ramaratissa@infoxchange.org>
2019-08-16 11:34:34 +1000

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='django_elastic_appsearch',
-    version='0.2.3',
+    version='0.3.0',
     description="""Integrate your Django Project with Elastic App Search with ease.""",
     long_description=readme + '\n\n' + history,
     author='Rasika Amaratissa',
