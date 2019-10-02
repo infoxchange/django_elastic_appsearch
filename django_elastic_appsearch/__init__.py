fe5dccff42d562f13f4f2feac35f327df635037e
Rasika Amaratissa <rasika.am@gmail.com>
2019-10-02 11:12:02 +1000

    Bump version: 0.5.3 → 0.5.4

--- django_elastic_appsearch/__init__.py
+++ django_elastic_appsearch/__init__.py
@@ -3,4 +3,4 @@
 # pylint:disable=invalid-name
 # Django requires `default_app_config`
 default_app_config = 'django_elastic_appsearch.apps.DjangoAppSearchConfig'
-__version__ = '0.5.3'
+__version__ = '0.5.4'
