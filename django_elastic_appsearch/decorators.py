87d7333ac8201895417a11fbdaa69c726e2d64b6
Bryce James <bryce@idscreates.com>
2020-09-09 18:38:00 -0400

    Add decorators.py with disable_auto_indexing

---
+++ django_elastic_appsearch/decorators.py
@@ -0,0 +1,52 @@
+from contextlib import ContextDecorator
+
+from django.apps import apps
+from django.db.models.signals import post_save, post_delete
+
+from django_elastic_appsearch.signals import post_save_receiver
+from django_elastic_appsearch.signals import post_delete_receiver
+
+
+class disable_auto_indexing(ContextDecorator):
+    """Context decorator to disable auto indexing signals.
+
+    Can be used as a context manager:
+    >>> with disable_auto_indexing('Model'):
+    >>>     obj.save()
+
+    Can be used as a method decorator:
+    >>> @disable_auto_indexing('Model')
+    >>> some_operation()
+    """
+
+    def __init__(self, model):
+        self.model = model
+        self.auto_indexing = apps.get_app_config(
+            'django_elastic_appsearch'
+        ).auto_indexing
+
+    def __enter__(self):
+        """Disable post save/delete signals."""
+        post_save.disconnect(
+            post_save_receiver,
+            sender=self.model
+        )
+        post_delete.disconnect(
+            post_delete_receiver,
+            sender=self.model
+        )
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Re-enable post save/delete signals."""
+
+        if not self.auto_indexing:
+            return
+
+        post_save.connect(
+            post_save_receiver,
+            sender=self.model
+        )
+        post_delete.connect(
+            post_delete_receiver,
+            sender=self.model
+        )
