2e0697c4ff873319847bad23d88a42d6f341bb97
Rasika Amaratissa <rasika.am@gmail.com>
2020-11-26 10:54:23 +1100

    Fix the supported Django version lists in the setup module

--- setup.py
+++ setup.py
@@ -65,10 +65,9 @@ setup(
     keywords='django_elastic_appsearch',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
