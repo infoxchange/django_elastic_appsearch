95e9d1ced1af4cc6225ec23ad841f01572e2784e
Rasika Amaratissa <rasika.am@gmail.com>
2020-11-27 11:40:13 +1100

    Implement helper method to get the enterprise search `AppSearch` client

--- django_elastic_appsearch/clients.py
+++ django_elastic_appsearch/clients.py
@@ -2,6 +2,7 @@
 
 from django.apps import apps
 from elastic_app_search import Client
+from elastic_enterprise_search import AppSearch
 
 
 def get_api_v1_client():
@@ -17,3 +18,18 @@ def get_api_v1_client():
         base_endpoint=base_endpoint,
         use_https=use_https
     )
+
+def get_api_v1_enterprise_search_client():
+    """Return the enterprise-search appsearch client."""
+    config = apps.get_app_config('django_elastic_appsearch')
+
+    appsearch_host = config.appsearch_host
+    api_key = config.api_key
+    use_https = config.use_https
+
+    url = '{}://{}'.format(
+        'https' if use_https else 'http',
+        appsearch_host
+    )
+
+    return AppSearch(url, http_auth=api_key)
