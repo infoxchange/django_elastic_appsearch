ea90d4c971ab53aabc7c7e55ad618efb17e24f29
Francisco Lara <flara@infoxchange.org>
2022-09-08 08:55:42 +1000

    Use bearer auth for AppSearch client

--- django_elastic_appsearch/clients.py
+++ django_elastic_appsearch/clients.py
@@ -41,4 +41,4 @@ def get_api_v1_enterprise_search_client():
         appsearch_host
     )
 
-    return AppSearch(url, http_auth=api_key, **config.extra_config_options)
+    return AppSearch(url, bearer_auth=api_key, **config.extra_config_options)
